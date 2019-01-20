e752253f33131b32a5d8b5014d4a83a97a60c7c2
J <iv@Js-MacBook-Pro.broadband>
2019-01-20 21:00:16 +0000

    The time is now

---
+++ README.md
@@ -0,0 +1,73 @@
+# Future of Blockchain : LBL DAO Entry
+
+An implementation of a self-sovereign organization, designed by and for project participants.
+
+[Project proposal](./self-sovereign-org-proposal.pdf).
+
+## Components / Deliverables
+
+### Whitepaper
+
+A research paper describing the problem, defining terms, critically exploring solutions or possible implementations and laying out future research.
+
+### Prototype
+
+A functional prototype deployed on an ethereum testnet, with some traditional back-end architecture (server, off-chain databases).
+
+Major question: privacy. Are all on-chain data / IPFS-addressed content encrypted? With which key?
+  Nucypher FHE library and proxy re-encryption scheme
+
+**Users**
+
+Participants
+Clients / customers
+Arbitrators
+...?
+
+#### Front end
+
+**Interfaces**
+
+Deploy project
+Participants accept terms (multisig to initiate project)
+Accept / distribute funds (is this an interface?)
+  Payment
+  Budget allocation
+Submit evidence / deliverable
+  Evidence review (multisig)
+    approve - execute terms
+    revise
+    contest / reject - divert to arbitration
+  Should trigger notification to reviewers
+Project dashboard (who can view?)
+  Users (contact details?)
+  Assets (i.e. submissions)
+  Tasks (upcoming, awaiting review, approved, disputed)
+    Calendar view
+    Owner view
+    Category view
+    Status view
+
+React
+web3.js
+
+#### Back end
+
+**EVM**
+
+deploy project
+user accept role
+accept / disburse funds
+create / update task (ongoing - agile)
+submit / review evidence
+(see rough [sso.sol]('./contracts/sso.sol'))
+
+Solidity
+
+#### Traditional server
+
+Server hosting website, cache of project-relevant files (IPFS node? SQL or mongo database?)
+
+Node
+mongodb
+web3.js
---
+++ contracts/sso.sol
@@ -0,0 +1,76 @@
+// Pseudocode / rough outline of contracts for the self-sovereign organization
+
+// Data
+string[] statuses; // potential statuses for tasks
+mapping (address => participant) participants;
+mapping (address => investor) investors;
+
+struct participant {
+  address ethaddress;
+  float equity;
+  // DID ?
+}
+
+struct Investor {
+  address ethaddress;
+  uint amtinvested;
+}
+
+struct IpfsHash {
+  bytes32 hash;
+  uint hashSize;
+}
+
+struct Task {
+  address owner;
+  uint duetime;
+  IpfsHash requirements; // ipfs address to file detailing requirements
+  IpfsHash evidence; // address to file (encrypted?)
+  bool approved;
+  string status;
+  float budget;
+  address budgetrecipient; // more sophisticated - address and proportion / absolute number of budget?
+  task[] dependencies; // tasks that must be complete prior to this task
+  address[] reviewers; // reviewers who need to approve task - may need more nuance - weighted votes, say
+  // more here about arbitrator address maybe?
+}
+
+// Some structure superior to Task - Sprint or Milestone? 
+
+// contract addresses for payment wallets - automatically disburse
+
+function SSO () {
+  // constructor function
+  // perhaps define list of approved investors
+}
+
+function deploy () {
+  // multisig / funding requirement to deploy project
+
+}
+
+function invest () payable {
+  amount += msg.value;
+  // update
+}
+
+function disburse () {
+  // disburse funds to appropriate address
+}
+
+function submit () {
+  // accepts info about participant, evidence
+  // Checks if compliant with task requirements
+  // Updates task, triggers review process?
+}
+
+function review (address reviewer, bool approved, string reasoning) {
+  // for each reviewer to submit
+  // string reasoning is chance to provide feedback / reason, esp for rejection
+}
+
+function undeploy () {
+  // multisig (?) project termination function
+  // returns remaining funds proportionately to participants / investors per pre-set terms
+
+}
